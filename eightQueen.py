4a9f6b54a758651e81a73311daef0da0d3c27813
LordGoodman <201negative@sina.com>
2015-10-09 16:56:44 +0800

    Finally get it!
    spent two days on it, I hope i'm not wasting my time

--- eightQueen.py
+++ eightQueen.py
@@ -1,4 +1,6 @@
-﻿def print_cheast(lis):
+﻿global count
+count =0
+def print_cheast(lis):
 	print("----------------------")
 	for i in lis:
 		for j in range(8):
@@ -10,31 +12,47 @@
 	print("----------------------")
 			
 def cheast_placing(lis):
-		for k in range(8):
-			next_put=condition_cheack(k,len(lis),lis)
-			if next_put:
-				lis.append(next_put)
-				return cheast_placing(lis)
-			else :
-				return cheast_placing(lis.pop())
+		
+			#if len(lis) == 0:
+			#	lis.append(k)
+			#	cheast_placing(lis)
 				
-		if len(lis)==8:
-			return print_cheast(lis)
+			#else :
+		next=condition_cheack(len(lis),lis)#this is the inspiration of this program
+		for k in next:
+					lis.append(k)
+					if len(lis)==8:
+						global count
+						print_cheast(lis)
+					#	print lis
+						count +=1
+					cheast_placing(lis)
+					lis.pop()
+			
 			
 		
-def condition_cheack(i,lenght,lis):
-		if i not in lis:
-			if ((lis[lenght-1]+lenght-1)!=(i+lenght-2)) and (abs(lis[lenght-1]-lenght+1)!=abs(i-lenght+2)):
-				if (abs(lis[lenght-1]-lenght+1)!=abs(i-lenght)) and ((lis[lenght-1]+lenght-1)!=(i+lenght)):
-					if i !=None :
-						return i	
-
-def init_list(lis):
-	for k in range(8):
-		lis.append(k)
-		cheast_placing(lis)
-		lis=[]			
 		
+		
+def condition_cheack(lenght,lis):#对角线上也不能有相同
+	next=[]#可行解数组
+	for i in range(8):
+		times=0#times for testing the next level,if get len(lis) times then it work
+		if i not in lis:
+			for k in range(lenght):
+				if ((lis[k]+k)!=(i+lenght)) and (abs(i-lis[k])!=abs(lenght-k)):#this is the most important part in this program
+	#the formula abs(i-lis[k])!=abs(lenght-k) make sure that every queen can't find another queen in her diagonal line 
+					#if (abs(lis[lenght-1]-lenght+1)!=abs(i-lenght)) and ((lis[lenght-1]+lenght-1)!=(i+lenght)):
+						if i !=None :
+							times +=1
+							if times==len(lis):
+							 next.append(i)
+								#print next
+	return next
+						
 if __name__ == "__main__":
-	lis = []
-	init_list(lis)
+ lis=[]	
+ for i in range(8):
+	lis.append(i)
+	cheast_placing(lis)
+	del lis[:]
+print count
